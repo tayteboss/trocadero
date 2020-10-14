a829e1fcca23a767805ae0997e2f7a4526187dfb
Tayte Boss <boss.tayte@gmail.com>
2020-10-14 19:47:20 +1100

    setup source sanity plugin for gatsby

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,10 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
+require('dotenv').config({
+  path: '.env'
+});
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'v1bojt31',
+        dataset: 'production',
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
---
+++ web/src/pages/posts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Theme from '../shared/theme';
+import GlobalStyle from '../shared/baseCSS';
+import H1 from '../elements/typography/H1';
+import Button from '../elements/buttons/Button';
+
+export default function Home() {
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <H1>Hello world!</H1>
+    </ThemeProvider>
+  )
+};
